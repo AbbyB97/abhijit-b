569f18e48f905082ed0a38597ee0caf0b79026cc
Abhijit <abhijit.bansode@learnq.ai>
2022-06-08 20:08:58 +0530

    added basi header chakra template

---
+++ src/components/home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroWidget from "../../widgets/homeWidgets/hero";
+
+const index = () => {
+  return (
+    <>
+      <HeroWidget />
+    </>
+  );
+};
+
+export default index;
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Text,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import type { NextPage } from "next";
+import HomeComponent from "../components/home";
+const Home: NextPage = () => {
+  return <HomeComponent />;
+};
+
+export default Home;
