f6930cd19d4b13ba4ec9d7313d0ea3146ec03ce9
Abhijit <abhijit.bansode@learnq.ai>
2022-06-10 18:04:57 +0530

    added page dissolve transition and image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider, createLocalStorageManager } from '@chakra-ui/react'
+import { ChakraProvider, createLocalStorageManager } from "@chakra-ui/react";
 import theme from "theme";
-const manager = createLocalStorageManager('ab-key')
-
+import ABDissolve from "components/shared/ab-dissolve";
+import {  useRouter } from "next/router";
+const manager = createLocalStorageManager("ab-key");
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const route = useRouter();
   return (
     <ChakraProvider colorModeManager={manager} theme={theme}>
-      <Component {...pageProps} />
+      <ABDissolve duration={1.5} key={route.route}>
+        <Component {...pageProps} />
+      </ABDissolve>
     </ChakraProvider>
   );
 }
